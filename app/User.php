f8968e7125b61834a8a0d6f0e10d3192d2fc84f7
sasa <fekete.sandor@revotech.hu>
2019-11-08 21:21:57 +0100

    Display answers for question

--- app/User.php
+++ app/User.php
@@ -49,4 +49,11 @@ class User extends Authenticatable
     public function answers() {
         return $this->hasMany(Answer::class);
     }
+
+    public function getAvatarAttribute() {
+        $email = $this->email;
+        $size = 32;
+
+        return $grav_url = "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
+    }
 }
